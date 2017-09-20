c440fa6d837adf82295cbdee886f130fddb983f1
BrandonRomano <BrandonRRomano@gmail.com>
2017-09-20 17:21:36 -0400

    Add Slug to state.  Also adds docs

--- emitter.go
+++ emitter.go
@@ -1,5 +1,7 @@
 package fsm
 
+// Emitter is a generic interface to output arbitrary data.
+// Emit is generally called from State.EntryAction.
 type Emitter interface {
 	Emit(interface{}) error
 }
--- state.go
+++ state.go
@@ -1,6 +1,8 @@
 package fsm
 
+// State represents an individual state in a larger state machine
 type State struct {
+	Slug        string
 	EntryAction func() error
 	Transition  func(interface{}) *State
 }
